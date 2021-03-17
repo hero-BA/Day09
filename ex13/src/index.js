6def1333f9fa44bc28d792a360516e87bd1ef196
hero-BA <suad.hero@gmail.com>
2021-03-17 02:26:05 +0100

    Added exercise: While loops

---
+++ ex13/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+function inverseWhile() {
+    var i = 5;
+    var fiveNumbers = "";
+    while(i >= 0) {
+        fiveNumbers += i;
+        if(i > 0){
+            fiveNumbers += ",";
+        }
+        i--;
+    }
+    return fiveNumbers;
+}
+// Only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
